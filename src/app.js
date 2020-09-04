fd479d258c281782862b62a85f11c10b2b5b335e
Jay <chairit@parbpim.com>
2020-09-04 17:52:26 +0700

    Init commit

---
+++ src/app.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs');
+const geocode = require('./utils/geocode')
+const forcast = require('./utils/forcast')
+
+const app = express()
+//console.log(path.join(__dirname,'../public'))
+
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialspath = path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine and view location
+app.set('views',viewsPath)
+app.set('view engine','hbs')
+hbs.registerPartials(partialspath)
+
+
+//Setup static directory 
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Jay Benice'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title: 'Help',
+        helpText: 'Hang me oh hang me'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: 'About'
+    })
+})
+app.get('/weather',(req,res)=>{
+    const address = req.query.address;
+    if(!address){
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    geocode(address,(error,{latitude,longitude,location}={})=>{
+        if(error){
+            return res.send({error})
+        }
+        forcast(latitude,longitude, (error,forcastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forcast: forcastData,
+                location,
+                address
+            })
+        })
+    })
+
+})
+app.get('help/*',(req,res)=>{
+    res.render('404',{
+        title: '404',
+        errorMessage: 'Help article not found'
+    })
+})
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title: '404',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+})
---
+++ src/utils/forcast.js
@@ -0,0 +1,25 @@
+const request = require('request')
+
+const weather = (latitude,longitude,callback)=>{
+    url = 'http://api.weatherstack.com/current?access_key=415af248b120b405d036e1965f38a5a7&query='+latitude+','+longitude+'&units=m'
+    request({url, json: true}, (error,{body})=>{
+        if(error){
+            callback('Unable to connect to weather service!',undefined)
+        }else if(body.error){
+            callback('Uable to find location',undefined)
+        }else{
+            const currentTemp = body.current.temperature
+            const feelTemp = body.current.feelslike
+            const description = body.current.weather_descriptions[0];
+            callback(undefined,`${description}. It is currently ${currentTemp} degrees out. It feels like ${feelTemp} degrees out.` );
+        }
+        
+    })
+        
+}
+
+module.exports = weather
+
+// const url = 
+
+// 
