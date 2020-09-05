d993c2870d40963657028e19fccf98b7d934698a
Jay <chairit@parbpim.com>
2020-09-05 16:20:01 +0700

    Fix PORT to capital

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const geocode = require('./utils/geocode')
 const forcast = require('./utils/forcast')
 
